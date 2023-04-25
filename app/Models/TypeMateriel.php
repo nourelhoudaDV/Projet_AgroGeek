737594fc2afce60a66ad543a5f12d923181bd27e
Dr.Pvssy <medchihab651@gmail.com>
2023-04-25 04:39:57 +0200

    mregla hh

--- app/Models/TypeMateriel.php
+++ app/Models/TypeMateriel.php
@@ -5,15 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TypeMateriel extends Model
+class TypesMateriel extends Model
 {
     use HasFactory;
+
     protected $table = 'typesMateriel';
     protected $primaryKey = 'idTM';
-    protected $fillable = ['nom', 'description', 'techniqueA'];
-    
+    public $timestamps = false;
+    protected $fillable = [
+        'nom',
+        'description',
+        'techniqueA_id',
+    ];
+    protected $keyType = 'int';
+    public const PK = 'idTM';
+
     public function techniqueAgricole()
     {
-        return $this->belongsTo(TechniqueAgricole::class, 'techniqueA');
+        return $this->belongsTo(TechniqueAgricole::class, 'techniqueA_id');
     }
 }
