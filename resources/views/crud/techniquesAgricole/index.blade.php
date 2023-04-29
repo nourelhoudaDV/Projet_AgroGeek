b36651a7c09a061f1e888d3cf7cceb8982ed59b3
digital opium <test@ismail.com>
2023-04-29 12:15:08 +0100

    ismail_s task

--- resources/views/crud/techniquesAgricole/index.blade.php
+++ resources/views/crud/techniquesAgricole/index.blade.php
@@ -1,24 +1,25 @@
 @extends('layout.master')
 @include('include.blade-components')
 
-@section('page_title', '')
+@section('page_title', trans('pages/fermes.index_page_title'))
 @section('breadcrumb')
-    <x-group.bread-crumb page-tittle="{{ '' }}" :indexes="[
+    <x-group.bread-crumb page-tittle="{{ trans('pages/fermes.index_page_title') }}" :indexes="[
         [
-            'name' => '',
-            'route' => '',
+            'name' => trans('pages/fermes.index_page_title'),
+            'route' => route('fermes.index'),
         ],
     ]" />
 
 @endsection
+
 
 @section('content')
     @bind($collection)
         <x-table.data-table
         :actions="$actions"
         :heads="$heads"
-        edit-route="techniquesAgricole.show"
-        delete-route="techniquesAgricole.delete" />
+        edit-route="fermes.show"
+        delete-route="fermes.delete" />
     @endBinding
 
 
