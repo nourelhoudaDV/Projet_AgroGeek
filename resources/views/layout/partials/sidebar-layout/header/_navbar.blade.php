32c62ed46e620348da72c3780121bf1755453056
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-05-25 13:45:56 +0100

    les disgn du sidebar et header

--- resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
+++ resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
@@ -1,68 +1,6 @@
 <!--begin::Navbar-->
 <div class="app-navbar flex-shrink-0">
-    <!--begin::Search-->
-    <div class="app-navbar-item align-items-stretch ms-1 ms-lg-3">
-        @include(config('settings.KT_THEME_LAYOUT_DIR').'/partials/sidebar-layout/search/_dropdown')
-    </div>
-    <!--end::Search-->
-    <!--begin::Activities-->
-    <div class="app-navbar-item ms-1 ms-lg-3">
-        <!--begin::Drawer toggle-->
-        <div
-            class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px"
-            id="kt_activities_toggle">
-            <!--begin::Svg Icon | path: icons/duotune/general/gen032.svg-->
-            {!! getSvgIcon('duotune/general/gen032.svg', 'svg-icon svg-icon-1') !!}
-            <!--end::Svg Icon-->
-        </div>
-        <!--end::Drawer toggle-->
-    </div>
-    <!--end::Activities-->
-    <!--begin::Notifications-->
-    <div class="app-navbar-item ms-1 ms-lg-3">
-        <!--begin::Menu- wrapper-->
-        <div
-            class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px"
-            data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent"
-            data-kt-menu-placement="bottom-end">
-            <!--begin::Svg Icon | path: icons/duotune/general/gen022.svg-->
-            {!! getSvgIcon('duotune/general/gen022.svg', 'svg-icon svg-icon-1') !!}
-            <!--end::Svg Icon-->
-        </div>
-        @include('partials/menus/_notifications-menu')
-        <!--end::Menu wrapper-->
-    </div>
-    <!--end::Notifications-->
-    <!--begin::Chat-->
-    <div class="app-navbar-item ms-1 ms-lg-3">
-        <!--begin::Menu wrapper-->
-        <div
-            class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px position-relative"
-            id="kt_drawer_chat_toggle">
-            <!--begin::Svg Icon | path: icons/duotune/communication/com012.svg-->
-            {!! getSvgIcon('duotune/communication/com012.svg', 'svg-icon svg-icon-1') !!}
-            <!--end::Svg Icon-->
-            <span
-                class="bullet bullet-dot bg-success h-6px w-6px position-absolute translate-middle top-0 start-50 animation-blink"></span>
-        </div>
-        <!--end::Menu wrapper-->
-    </div>
-    <!--end::Chat-->
-    <!--begin::My apps links-->
-    <div class="app-navbar-item ms-1 ms-lg-3">
-        <!--begin::Menu wrapper-->
-        <div
-            class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px"
-            data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent"
-            data-kt-menu-placement="bottom-end">
-            <!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
-            {!! getSvgIcon('duotune/general/gen025.svg', 'svg-icon svg-icon-1') !!}
-            <!--end::Svg Icon-->
-        </div>
-        @include('partials/menus/_my-apps-menu')
-        <!--end::Menu wrapper-->
-    </div>
-    <!--end::My apps links-->
+
     <!--begin::Theme mode-->
     <div class="app-navbar-item ms-1 ms-lg-3">
         @include('partials/theme-mode/_main')
@@ -76,7 +14,9 @@
         <div class="cursor-pointer symbol symbol-35px symbol-md-40px"
              data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent"
              data-kt-menu-placement="bottom-end">
-            <img src="{{ image('avatars/300-1.jpg') }}" alt="user"/>
+             {{-- <img src="{{ image('avatars/300-1.jpg') }}" alt="user"/> --}}
+             <img src="{{ asset('assets/imgs/8.jpg')}}" alt="user" />
+
         </div>
         @include('partials/menus/_user-account-menu')
         <!--end::Menu wrapper-->
