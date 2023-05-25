1865af92f5778e7f8aa24c109dce3cebf9067642
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-05-25 13:31:22 +0100

    les view du parcelle

--- resources/views/crud/parcelle/create.blade.php
+++ resources/views/crud/parcelle/create.blade.php
@@ -3,10 +3,10 @@
 
 @section('page_title', 'Ajouter noveau parcelle')
 @section('breadcrumb')
-    <x-group.bread-crumb page-tittle='Ajouter noveau parcelle' :indexes="$pagesIndexes" {{-- [
-        ['name' => 'Parcelle', 'route' => route('fermes.index')],
-        ['name' => 'pages/parcelles.Ajouter noveau parcelle', 'current' => true],
-    ]"  --}} />
+    <x-group.bread-crumb page-tittle='Ajouter noveau parcelle' :indexes="[
+        // ['name' => 'liste de parcelles', 'route' => route('parcelles.index')],
+        ['name' => 'ajouter un parcelle', 'current' => true],
+    ]" />
 @endsection
 
 
@@ -14,12 +14,12 @@
 
     <x-form.form method="post" action="{{ route('parcelles.store') }}">
 
-        <x-form.card col="col-12 row" title='pages/parcelle.identification_parcelle'>
-
-            <x-form.input col="col-12 col-sm-6" name="codification" label="{{ trans('words.codification') }}" />
-            <x-form.select col="col-12 col-sm-6" name="Ferme" label="Ferme" :bind-with="[\App\Models\Ferme::allForSelect(), [\App\Models\Ferme::PK, 'ferme_name']]" />
-            <x-form.input col="col-12 col-sm-3" name="superficie" label="superficie" />
-            <x-form.radios col="col-12 col-sm-9" name="modeCulture" label="{{ trans('words.modeCulture') }}"
+        <x-form.card col="col-12 row" title='identification parcelle'>
+            <input type="hidden" name="Ferme" value="{{ $fermeId }}">
+            <x-form.select col="col-12 col-sm-6" name="typeSol" label="typeSol" :bind-with="[\App\Models\TypeSol::all(), [\App\Models\TypeSol::PK, 'vernaculaure']]" {{-- ['idTS', 'typeSol']]" --}} />
+            <x-form.input col="col-12 col-sm-6" name="codification" label="codification" />
+            <x-form.input required type="number" col="col-12 col-sm-3" name="superficie" label="superficie" />
+            <x-form.radios required col="col-12 col-sm-9" name="modeCulture" label="modeCulture"
                 :radios="[
                     [
                         'value' => 'S',
@@ -34,12 +34,11 @@
                         'label' => 'Etagère',
                     ],
                 ]" />
-            <x-form.text-area col="col-12 col-sm-6" name="topographie" label="topographie" />
             <x-form.input col="col-12 col-sm-3" name="pente" label="pente" />
             <x-form.input col="col-12 col-sm-3" name="pierosite" label="pierosite" />
             <x-form.input col="col-12 col-sm-6" name="gps" label="gps" />
+            <x-form.text-area col="col-12 col-sm-12" name="topographie" label="topographie" />
             <x-form.text-area col="col-12 col-sm-12" name="description" label="description" />
-            <x-form.select col="col-12 col-sm-6" name="typeSol" label="typeSol" :bind-with="[\App\Models\TypeSol::allForSelect(), [\App\Models\TypeSol::PK, 'typesol_name']]" {{-- ['idTS', 'typeSol']]" --}} />
 
             <div class="col-12 mt-5">
                 <x-form.button />
--- resources/views/crud/parcelle/edit.blade.php
+++ resources/views/crud/parcelle/edit.blade.php
@@ -1,16 +1,14 @@
 @extends('layout.master')
 @include('include.blade-components')
 
-@section('page_title', trans('pages/parcelle.update_parcelle'))
+@section('page_title', 'Modifier la parcelle')
 @section('breadcrumb')
     <x-group.bread-crumb
-        page-tittle="{{ trans('pages/parcelle.edit_page_title') }}"
-        :indexes="$pagesIndexes"
-    {{-- //     [
-    //     ['name' => trans('words.ferme'), 'route' => route('fermes.index')],
-    //     ['name' => trans('pages/parcelle.update_user'), 'current' => true],
-    // ]" --}}
-     />
+        page-tittle="Modifier la parcelle"
+        :indexes=:indexes="[
+            ['name' => 'liste de parcelles', 'route' => route('parcelles.index')],
+            ['name' => 'modifier le parcelle', 'current' => true],
+        ]" />
 @endsection
 
 
@@ -21,49 +19,38 @@
             method="post"
             action="{{ route('parcelles.update', $model[$model::PK]) }}"
         >
-            <x-form.card col="col-12 row" title='pages/parcelle.identification_parcelle'>
-                @bind($model)
-                <x-form.input col="col-12 col-sm-6" name="codification" label="{{ trans('words.codification') }}" />
-                <x-form.select
-                    col="col-12 col-sm-6"
-                    name="Ferme"
-                    label="Ferme"
-                    {{-- :bind-with="[\App\Models\Ferme::all(), ['idF', 'Ferme']]" /> --}}
-                    :bind-with="[\App\Models\Ferme::allForSelect(), [\App\Models\Ferme::PK , 'ferme_name']]"
-                />
-                <x-form.input col="col-12 col-sm-3" name="superficie" label="superficie" />
-                <x-form.radios col="col-12 col-sm-9" name="modeCulture" label="{{ trans('words.modeCulture') }}"
-                    :radios="[
-                        [
-                            'value' => 'S',
-                            'label' => 'Simple',
-                        ],
-                        [
-                            'value' => 'M',
-                            'label' => 'Mixte',
-                        ],
-                        [
-                            'value' => 'E',
-                            'label' => 'Etagère',
-                        ],
-                    ]"
-                />
-                <x-form.text-area col="col-12 col-sm-6" name="topographie" label="topographie" />
-                <x-form.input col="col-12 col-sm-3" name="pente" label="pente" />
-                <x-form.input col="col-12 col-sm-3" name="pierosite" label="pierosite" />
-                <x-form.input col="col-12 col-sm-6" name="gps" label="gps" />
-                <x-form.text-area col="col-12 col-sm-12" name="description" label="description" />
-                <x-form.select col="col-12 col-sm-6"
-                    name="typeSol"
-                    label="typeSol"
-                    :bind-with="[\App\Models\TypeSol::allForSelect(), [\App\Models\TypeSol::PK , 'typesol_name']]" />
+        @bind($model)
+        <x-form.card col="col-12 row" title='identification parcelle'>
+            <x-form.select col="col-12 col-sm-6" name="typeSol" label="typeSol" :bind-with="[\App\Models\TypeSol::all(), [\App\Models\TypeSol::PK, 'vernaculaure']]" {{-- ['idTS', 'typeSol']]" --}} />
+            <x-form.input col="col-12 col-sm-6" name="codification" label="codification" />
+            <x-form.input col="col-12 col-sm-3" name="superficie" label="superficie" />
+            <x-form.radios col="col-12 col-sm-9" name="modeCulture" label="modeCulture"
+                :radios="[
+                    [
+                        'value' => 'S',
+                        'label' => 'Simple',
+                    ],
+                    [
+                        'value' => 'M',
+                        'label' => 'Mixte',
+                    ],
+                    [
+                        'value' => 'E',
+                        'label' => 'Etagère',
+                    ],
+                ]" />
+            <x-form.input col="col-12 col-sm-3" name="pente" label="pente" />
+            <x-form.input col="col-12 col-sm-3" name="pierosite" label="pierosite" />
+            <x-form.input col="col-12 col-sm-6" name="gps" label="gps" />
+            <x-form.text-area col="col-12 col-sm-12" name="topographie" label="topographie" />
+            <x-form.text-area col="col-12 col-sm-12" name="description" label="description" />
+
+            <div class="col-12 mt-5">
+                <x-form.button />
+            </div>
+
+        </x-form.card>
                 @endBinding
-
-                <div class="col-12 mt-5">
-                    <x-form.button />
-                </div>
-
-            </x-form.card>
 
         </x-form.form>
     {{-- @endBinding --}}
