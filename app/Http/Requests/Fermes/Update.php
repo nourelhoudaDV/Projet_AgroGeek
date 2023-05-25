29999471a8bf917f2246cd535dc91d08067293c6
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-05-25 13:56:44 +0100

    les request du validation

--- app/Http/Requests/Fermes/Update.php
+++ app/Http/Requests/Fermes/Update.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Requests\Fermes;
 
+use App\Models\Ferme;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class Add extends FormRequest
+class Update extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -25,27 +26,35 @@ class Add extends FormRequest
     public function rules()
     {
 
-//        dd(
-//          $this->all()
-//        );
-
+        $id=request('idF');
 
 
         return   [
             'nomDomaine' => 'required|string|max:150',
             'fullNameG' => 'required|string|max:150',
-            'cin' => 'required|string|unique:fermes,cin|max:15',
+            
+            'cin' => ['required', 'max:10', 'string',  
+            function ($attribute, $value, $fail) use ($id) { 
+                $o = Ferme::query()    
+                                ->where(Ferme::PK, '!=', $id)                         
+                                ->where('cin', $value)                         
+                                ->first();                      
+                                if (isset($o)) {                         
+                                    $fail(__('validation.unique', ['attribute' => $attribute]));                     
+                                }                 
+                            }],
             'contactG' => 'nullable|string|max:15',
             'SAT' => 'nullable|string|max:15',
             'contactG' => 'nullable|string|max:15',
             'SAU' => 'nullable|string|max:15',
             'observation' => 'nullable|string|max:255',
-            'fixe' => 'nullable|string|max:15|regex:/^([0-9\s\-\+\(\)]*)$/',
-            'fax' => 'nullable|string|max:15|regex:/^([0-9\s\-\+\(\)]*)$/',
-            'GSM1' => 'nullable|string|max:15|regex:/^([0-9\s\-\+\(\)]*)$/',
-            'GSM2' => 'nullable|string|max:15|regex:/^([0-9\s\-\+\(\)]*)$/',
-            'email' => 'nullable|email|max:150',
-            'siteWeb' => 'nullable|url|max:150',
+            'fixe' => 'nullable|string|max:15',
+            'fax' => 'nullable|string|max:15',
+            'GSM1' => 'nullable|string|max:15',
+            'GSM2' => 'nullable|string|max:15',
+            'email' => 'nullable|string|max:150',
+            'siteWeb' => 'nullable|string|max:150',
+            'email' => 'nullable|string|max:150',
             'Douar' => 'nullable|string|max:100',
             'Cercle' => 'nullable|string|max:100',
             'province' => 'nullable|string|max:100',
@@ -54,8 +63,10 @@ class Add extends FormRequest
             'gps' => 'nullable|string|max:255',
             'ville' => 'nullable|string|max:100',
             'logo' =>   $this->image('logo') . 'image|mimes:jpeg,png,jpg|max:2048'
+
         ];
     }
+
     private function image($name): string
     {
         return $this->request->get("$name-preview") !== null ? 'nullable|' : 'nullable|';
